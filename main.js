45ea18b3ce014e053524742442831e6e94b5085d
tariba <tariqxa@gmail.com>
2022-11-03 15:10:04 +0000

    kata description completed

--- main.js
+++ main.js
@@ -2,4 +2,8 @@
 👉 Write your kata here!
 */
 
+//Write a Function that takes in a string containing a sentence and convert it into a valid camelCase word.
+
+
 //👉 Write the function your CodeWarriors will start with below here:
+
