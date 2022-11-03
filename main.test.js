7fc92ec535cdd0a756ea99d7f7a1ca0885d0593a
tariba <tariqxa@gmail.com>
2022-11-03 16:03:16 +0000

    all test completed

--- main.test.js
+++ main.test.js
@@ -1,40 +1,43 @@
 import { test, expect } from "@jest/globals";
 
-// Tests
+test(`Given a string of 'make me camel case', returns 'makeMeCamelCase'`, () => {
+  const actual = covertToCamelCase("make me camel case");
+  const expected = "makeMeCamelCase";
+  expect(actual).toBe(expected);
+});
 
-// lowercase, multiple words
-// test(`Given a string of 'make me camel case', returns 'makeMeCamelCase'`, () => {
-// 	const actual = covertToCamelCase("make me camel case");
-// 	const expected = "makeMeCamelCase";
-// 	expect(actual).toBe(expected);
-// });
+test(`Given a string of 'ALSO MAKE ME INTO CAMEL CASE', returns 'alsoMakeMeIntoCamelCase'`, () => {
+  const actual = covertToCamelCase("ALSO MAKE ME INTO CAMEL CASE");
+  const expected = "alsoMakeMeIntoCamelCase";
+  expect(actual).toBe(expected);
+});
 
-// uppercase, multiple words
-// test(`Given a string of 'ALSO MAKE ME INTO CAMEL CASE', returns 'alsoMakeMeIntoCamelCase'`, () => {
-// 	const actual = covertToCamelCase('ALSO MAKE ME INTO CAMEL CASE');
-// 	const expected = 'alsoMakeMeIntoCamelCase';
-// 	expect(actual).toBe(expected);
-// });
+test(`Given a string of 'AnD meEEeeE', returns 'andMeeeeee'`, () => {
+  const actual = covertToCamelCase("AnD meEEeeE");
+  const expected = "andMeeeeee";
+  expect(actual).toBe(expected);
+});
 
-// combination, multiple words
-// test(`Given a string of 'AnD meEEeeE', returns 'andMeeeeee'`, () => {
-// 	const actual = covertToCamelCase('AnD meEEeeE');
-// 	const expected = 'andMeeeeee';
-// 	expect(actual).toBe(expected);
-// });
+test(`Given a string of '', returns null`, () => {
+  const actual = covertToCamelCase("");
+  const expected = null;
+  expect(actual).toBe(expected);
+});
 
-// empty string
+test(`Given a string of 'HELLO', returns null`, () => {
+  const actual = covertToCamelCase("HELLO");
+  const expected = null;
+  expect(actual).toBe(expected);
+});
 
-function covertToCamelCase(string) {
-	return "andMeeeeee";
-}
+test(`Given a string of 'world', returns null`, () => {
+  const actual = covertToCamelCase("world");
+  const expected = null;
+  expect(actual).toBe(expected);
+});
 
-covertToCamelCase("AnD meEEeeE");
-
-// uppercase, single words
-
-// lowercase, single words
-
-// combination, single words
-
-// Implementation Stuff
+test(`Given a string of 'SoC', returns null`, () => {
+  const actual = covertToCamelCase("SoC");
+  const expected = null;
+  expect(actual).toBe(expected);
+});
