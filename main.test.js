8171867eaab320e202b1fe71bf2666e33024eb76
Steve Beecheno <iamhacktinium@gmail.com>
2022-11-03 15:52:46 +0000

    Wrote first three tests

--- main.test.js
+++ main.test.js
@@ -1,1 +1,40 @@
-//👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+
+// Tests
+
+// lowercase, multiple words
+// test(`Given a string of 'make me camel case', returns 'makeMeCamelCase'`, () => {
+// 	const actual = covertToCamelCase("make me camel case");
+// 	const expected = "makeMeCamelCase";
+// 	expect(actual).toBe(expected);
+// });
+
+// uppercase, multiple words
+// test(`Given a string of 'ALSO MAKE ME INTO CAMEL CASE', returns 'alsoMakeMeIntoCamelCase'`, () => {
+// 	const actual = covertToCamelCase('ALSO MAKE ME INTO CAMEL CASE');
+// 	const expected = 'alsoMakeMeIntoCamelCase';
+// 	expect(actual).toBe(expected);
+// });
+
+// combination, multiple words
+// test(`Given a string of 'AnD meEEeeE', returns 'andMeeeeee'`, () => {
+// 	const actual = covertToCamelCase('AnD meEEeeE');
+// 	const expected = 'andMeeeeee';
+// 	expect(actual).toBe(expected);
+// });
+
+// empty string
+
+function covertToCamelCase(string) {
+	return "andMeeeeee";
+}
+
+covertToCamelCase("AnD meEEeeE");
+
+// uppercase, single words
+
+// lowercase, single words
+
+// combination, single words
+
+// Implementation Stuff
